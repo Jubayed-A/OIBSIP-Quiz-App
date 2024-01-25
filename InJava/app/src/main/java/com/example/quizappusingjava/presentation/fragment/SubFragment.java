5ca2e440fc0c0be5a934564fe5e64d3b874f3ce6
Jubayed-A <learnigprograming@gmail.com>
2024-01-25 08:14:03 +0600

    SubFragment.java file updated with string file

--- InJava/app/src/main/java/com/example/quizappusingjava/presentation/fragment/SubFragment.java
+++ InJava/app/src/main/java/com/example/quizappusingjava/presentation/fragment/SubFragment.java
@@ -49,64 +49,64 @@ public class SubFragment extends Fragment {
         // Retrieve subcategory names and descriptions from string arrays
 
         // Accessing general sub-category and description arrays
-        String[] generalCategory = getResources().getStringArray(R.array.general_sub_category);
-        String[] generalDescription = getResources().getStringArray(R.array.general_sub_category_description);
+        String[] generalCategory = getResources().getStringArray(R.array.general_knowledge_sub_categories);
+        String[] generalDescription = getResources().getStringArray(R.array.general_knowledge_sub_categories_description);
         // Ensure the General arrays have the same length
         int generalLength = Math.min(generalCategory.length, generalDescription.length);
 
         // Accessing Science sub-category and description arrays
-        String[] scienceCategory = getResources().getStringArray(R.array.Science_sub_category);
-        String[] scienceDescription = getResources().getStringArray(R.array.Science_sub_category_description);
+        String[] scienceCategory = getResources().getStringArray(R.array.science_questions);
+        String[] scienceDescription = getResources().getStringArray(R.array.science_descriptions);
         int scienceLength = Math.min(scienceCategory.length, scienceDescription.length);
 
         // Accessing Technology sub-category and description arrays
-        String[] technologyCategory = getResources().getStringArray(R.array.Technology_sub_category);
-        String[] technologyDescription = getResources().getStringArray(R.array.Technology_sub_category_description);
+        String[] technologyCategory = getResources().getStringArray(R.array.technology_questions);
+        String[] technologyDescription = getResources().getStringArray(R.array.technology_descriptions);
         int technologyLength = Math.min(technologyCategory.length, technologyDescription.length);
 
         // Accessing Entertainment sub-category and description arrays
-        String[] entertainmentCategory = getResources().getStringArray(R.array.Entertainment_sub_category);
-        String[] entertainmentDescription = getResources().getStringArray(R.array.Entertainment_sub_category_description);
+        String[] entertainmentCategory = getResources().getStringArray(R.array.entertainment_questions);
+        String[] entertainmentDescription = getResources().getStringArray(R.array.entertainment_descriptions);
         int entertainmentLength = Math.min(entertainmentCategory.length, entertainmentDescription.length);
 
         // Accessing Sports sub-category and description arrays
-        String[] sportsCategory = getResources().getStringArray(R.array.Sports_sub_category);
-        String[] sportsDescription = getResources().getStringArray(R.array.Sports_sub_category_description);
+        String[] sportsCategory = getResources().getStringArray(R.array.sports_questions);
+        String[] sportsDescription = getResources().getStringArray(R.array.sports_descriptions);
         int sportsLength = Math.min(sportsCategory.length, sportsDescription.length);
 
         // Accessing Literature sub-category and description arrays
-        String[] literatureSubCategory = getResources().getStringArray(R.array.Literature_sub_category);
-        String[] literatureDescription = getResources().getStringArray(R.array.Literature_sub_category_description);
+        String[] literatureSubCategory = getResources().getStringArray(R.array.literature_questions);
+        String[] literatureDescription = getResources().getStringArray(R.array.literature_descriptions);
         int literatureLength = Math.min(literatureSubCategory.length, literatureDescription.length);
 
         // Accessing Arts and Culture sub-category and description arrays
-        String[] artsAndCultureSubCategory = getResources().getStringArray(R.array.Arts_and_Culture_sub_category);
-        String[] artsAndCultureDescription = getResources().getStringArray(R.array.Arts_and_Culture_sub_category_description);
+        String[] artsAndCultureSubCategory = getResources().getStringArray(R.array.arts_and_culture_questions);
+        String[] artsAndCultureDescription = getResources().getStringArray(R.array.arts_and_culture_descriptions);
         int artsAndCultureLength = Math.min(artsAndCultureSubCategory.length, artsAndCultureDescription.length);
 
         // Accessing Food and Drink sub-category and description arrays
-        String[] foodAndDrinkSubCategory = getResources().getStringArray(R.array.Food_and_Drink_sub_category);
-        String[] foodAndDrinkDescription = getResources().getStringArray(R.array.Food_and_Drink_sub_category_description);
+        String[] foodAndDrinkSubCategory = getResources().getStringArray(R.array.food_and_drink_questions);
+        String[] foodAndDrinkDescription = getResources().getStringArray(R.array.food_and_drink_descriptions);
         int foodAndDrinkLength = Math.min(foodAndDrinkSubCategory.length, foodAndDrinkDescription.length);
 
         // Accessing Mathematics sub-category and description arrays
-        String[] mathematicsSubCategory = getResources().getStringArray(R.array.Mathematics_sub_category);
-        String[] mathematicsDescription = getResources().getStringArray(R.array.Mathematics_sub_category_description);
+        String[] mathematicsSubCategory = getResources().getStringArray(R.array.mathematics_questions);
+        String[] mathematicsDescription = getResources().getStringArray(R.array.mathematics_descriptions);
         int mathematicsLength = Math.min(mathematicsSubCategory.length, mathematicsDescription.length);
 
         // Accessing Language and Words sub-category and description arrays
-        String[] languageAndWordsSubCategory = getResources().getStringArray(R.array.Language_and_Words_sub_category);
-        String[] languageAndWordsDescription = getResources().getStringArray(R.array.Language_and_Words_sub_category_description);
+        String[] languageAndWordsSubCategory = getResources().getStringArray(R.array.language_and_words_questions);
+        String[] languageAndWordsDescription = getResources().getStringArray(R.array.language_and_words_descriptions);
         int languageAndWordsLength = Math.min(languageAndWordsSubCategory.length, languageAndWordsDescription.length);
 
         // Accessing Nature and Environment sub-category and description arrays
-        String[] natureAndEnvironmentSubCategory = getResources().getStringArray(R.array.Nature_and_Environment_sub_category);
-        String[] natureAndEnvironmentDescription = getResources().getStringArray(R.array.Nature_and_Environment_sub_category_description);
+        String[] natureAndEnvironmentSubCategory = getResources().getStringArray(R.array.nature_and_environment_questions);
+        String[] natureAndEnvironmentDescription = getResources().getStringArray(R.array.nature_and_environment_descriptions);
         int natureAndEnvironmentLength = Math.min(natureAndEnvironmentSubCategory.length, natureAndEnvironmentDescription.length);
 
         // Accessing Mythology sub-category and description arrays
-        String[] mythologySubCategory = getResources().getStringArray(R.array.Mythology_sub_category);
-        String[] mythologyDescription = getResources().getStringArray(R.array.Mythology_sub_category_description);
+        String[] mythologySubCategory = getResources().getStringArray(R.array.mythology_questions);
+        String[] mythologyDescription = getResources().getStringArray(R.array.mythology_descriptions);
         int mythologyLength = Math.min(mythologySubCategory.length, mythologyDescription.length);
 
 
